2a5290cd88aa70883d528bc38000b8043d351969
Karol Kochaski <karolkochanski@gmail.com>
2020-02-14 22:33:53 +0100

    Add setup of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="logo">
+          OfficeSupplies
+        </div>
+        <div className="search-place">
+          <input type="text" name="search-input"/>
+          <input type="submit" name="Search"  />
+        </div>
       </header>
+      <div className="menu">
+
+      </div>
+      {/*CENTER CONTAINER*/}
+      <div className="main-container">
+      </div>
+
+      {/*FOOTER*/}
+      <div className="App-footer">
+      </div>
     </div>
   );
 }
